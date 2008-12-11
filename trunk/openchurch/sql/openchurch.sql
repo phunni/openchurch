d5fd9556a937d46559b3f3ca9ef24537b70e0a64
phunnimonkey <phunnimonkey@5ede4246-c789-11dd-a2ab-b939405fd53f>
2008-12-11 15:56:40 +0000

    added openchurch_users table

--- trunk/openchurch/sql/openchurch.sql
+++ trunk/openchurch/sql/openchurch.sql
@@ -6,3 +6,10 @@ drop database openchurch;
 create database openchurch;
 
 USE openchurch;
+
+create table openchurch_users (
+    id int primary key,
+    userid varchar(50) not null,
+    preferredNames varchar(500) not null,
+    surname varchar(500) not null,
+)
