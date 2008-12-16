5b4873dc94213ebc39142585a9b080c7ea34f775
phunnimonkey <phunnimonkey@5ede4246-c789-11dd-a2ab-b939405fd53f>
2008-12-16 15:26:07 +0000

    Began developing the User entity and supporting classes - including some classes to facilitate the unit testing of entities... Currently not DB generic enough...

---
+++ trunk/openchurch/src/modules/core/users/tests/uk/co/monkeypower/openchurch/core/users/entities/UserTest.java
@@ -0,0 +1,57 @@
+package uk.co.monkeypower.openchurch.core.users.entities;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.sql.DataSource;
+
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+
+public class UserTest {
+    
+    private static DataSource dataSource;
+    
+    @BeforeClass
+    public static void setUpJNDI() throws NamingException {	
+	System.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.naming.java.javaURLContextFactory");
+	System.setProperty(Context.URL_PKG_PREFIXES, "org.apache.naming");
+	InitialContext initCtx = new InitialContext();
+	initCtx.createSubcontext("java:");
+	initCtx.createSubcontext("java:comp");
+	initCtx.createSubcontext("java:comp/env");
+	initCtx.createSubcontext("java:comp/env/jdbc");
+
+	dataSource = new OpenChurchUtilityDatasourceForTesting();
+	initCtx.bind("java:comp/env/jdbc/openchurch", dataSource);
+    }
+    
+    @Test
+    public void createUser(){
+	EntityManager manager = Persistence.createEntityManagerFactory("openchurch_users").createEntityManager();
+	User user = new User();
+	user.setId(0);
+	user.setUsername("test-user");
+	user.setPreferredNames("test-name");
+	user.setSurname("test-surname");
+	manager.getTransaction().begin();
+	manager.persist(user);
+	manager.getTransaction().commit();
+    }
+    
+    @After
+    public void cleanUp(){
+	EntityManager manager = Persistence.createEntityManagerFactory("openchurch_users").createEntityManager();
+	Query deleteQuery = manager.createQuery("delete from User u where u.username = 'test-user'");
+	manager.getTransaction().begin();
+	deleteQuery.executeUpdate();
+	manager.getTransaction().commit();
+    }
+
+}
