8fe86850ebdec5fee13b2d521eb98bb09a0ee488
phunnimonkey <phunnimonkey@5ede4246-c789-11dd-a2ab-b939405fd53f>
2009-01-13 12:43:32 +0000

    Added telephone numbers to the User entity

--- openchurch/sql/openchurch.sql
+++ openchurch/sql/openchurch.sql
@@ -9,15 +9,18 @@ USE openchurch;
 
 create table id_generator (
     id_name varchar(50) primary key,
-    id_value long not null
+    id_value bigint not null
 )
 
 insert into id_generator values ('INV_GEN', 0);
 
 create table openchurch_users (
-    id int primary key,
+    id bigint primary key,
     username varchar(50) not null unique,
     preferredNames varchar(500) not null,
     surname varchar(500) not null,
-    emailAddress varchar(250) not null
+    emailAddress varchar(250) not null,
+    homeTelNumber varchar(100),
+    workTelNumber varchar(100),
+    mobileTelNumber varchar(100)
 )
