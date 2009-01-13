b1f86a2401e0c7230ced632ef6e9b2978eb589f4
phunnimonkey <phunnimonkey@5ede4246-c789-11dd-a2ab-b939405fd53f>
2009-01-13 12:01:49 +0000

    Created the table to generate id's.  This table will also be used by all other entities...

--- openchurch/sql/openchurch.sql
+++ openchurch/sql/openchurch.sql
@@ -7,6 +7,13 @@ create database openchurch;
 
 USE openchurch;
 
+create table id_generator (
+    id_name varchar(50) primary key,
+    id_value long not null
+)
+
+insert into id_generator values ('INV_GEN', 0);
+
 create table openchurch_users (
     id int primary key,
     username varchar(50) not null unique,
