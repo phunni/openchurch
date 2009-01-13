6cf1772f5104fbad796682f564df4d379c39056d
phunnimonkey <phunnimonkey@5ede4246-c789-11dd-a2ab-b939405fd53f>
2009-01-13 12:01:15 +0000

    Changed the User entity to use a table to generate id's.  This table will also be used by all other entities...

--- trunk/openchurch/src/modules/core/users/src/uk/co/monkeypower/openchurch/core/users/entities/User.java
+++ trunk/openchurch/src/modules/core/users/src/uk/co/monkeypower/openchurch/core/users/entities/User.java
@@ -1,26 +1,34 @@
 package uk.co.monkeypower.openchurch.core.users.entities;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.TableGenerator;
 
 import uk.co.monkeypower.openchurch.core.users.exception.UserAttributeValidationException;
 
 @Entity
 @Table(name="openchurch_users")
-public class User {
+public class User implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
     
     @Id
-    private int id;
+    @GeneratedValue(generator="IdTable")
+    @TableGenerator(name="IdTable", allocationSize=2, table = "id_generator", pkColumnName="id_name", valueColumnName="id_value", pkColumnValue="INV_GEN")
+    private long id;
     private String username;
     private String preferredNames;
     private String surname;
     private String emailAddress;
     
-    public int getId() {
+    public long getId() {
         return id;
     }
-    public void setId(int id) {
+    public void setId(long id) {
         this.id = id;
     }
     public String getUsername() {
