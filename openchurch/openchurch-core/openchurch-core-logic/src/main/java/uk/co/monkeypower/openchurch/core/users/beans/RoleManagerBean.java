c76ea65eebd15fa92418249a279052016543a233
phunnimonkey <phunnimonkey@5ede4246-c789-11dd-a2ab-b939405fd53f>
2009-03-04 13:53:49 +0000

    Added the Session Bean for controlling Role entities

---
+++ openchurch/openchurch-core/openchurch-core-logic/src/main/java/uk/co/monkeypower/openchurch/core/users/beans/RoleManagerBean.java
@@ -0,0 +1,36 @@
+package uk.co.monkeypower.openchurch.core.users.beans;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+
+import uk.co.monkeypower.openchurch.core.users.entities.Role;
+
+public class RoleManagerBean implements RoleManager {
+    
+    @PersistenceContext(name="openchurch_users")
+    private EntityManager entityManager;
+    
+    public void createRole(Role roleToBeCreated) {
+	entityManager.persist(roleToBeCreated);
+    }
+
+    public List<Role> getAllRoles() {
+	Query getAllRolesQuery = entityManager.createQuery("select r from Role r");
+	return getAllRolesQuery.getResultList();
+    }
+
+    public Role getRole(String roleTitle) {
+	Query getRoleByTitleQuery = entityManager.createQuery("select r from Role r where r.title = ?1");
+	getRoleByTitleQuery.setParameter(1, roleTitle);
+	return (Role) getRoleByTitleQuery.getSingleResult();
+    }
+
+    public void removeRole(Role role) {
+	entityManager.remove(role);
+    }
+
+}
