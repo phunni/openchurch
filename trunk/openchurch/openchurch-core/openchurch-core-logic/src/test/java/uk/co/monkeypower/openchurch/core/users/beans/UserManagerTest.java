76934d052d617e55d5aae1a37b047fa0e95a1aa9
phunnimonkey <phunnimonkey@5ede4246-c789-11dd-a2ab-b939405fd53f>
2009-03-04 10:44:59 +0000

    Improved and (hopefully) completed the UserManager Session Bean

--- trunk/openchurch/openchurch-core/openchurch-core-logic/src/test/java/uk/co/monkeypower/openchurch/core/users/beans/UserManagerTest.java
+++ trunk/openchurch/openchurch-core/openchurch-core-logic/src/test/java/uk/co/monkeypower/openchurch/core/users/beans/UserManagerTest.java
@@ -1,17 +1,11 @@
 package uk.co.monkeypower.openchurch.core.users.beans;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.persistence.EntityManager;
-import javax.persistence.Persistence;
-import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-import uk.co.monkeypower.openchurch.core.users.beans.OpenChurchUtilityDatasourceForTesting;
-import uk.co.monkeypower.openchurch.core.users.beans.exceptions.UserManagementException;
+import uk.co.monkeypower.openchurch.core.users.entities.Role;
 import uk.co.monkeypower.openchurch.core.users.entities.User;
 import static org.junit.Assert.*;
 
@@ -19,57 +13,34 @@ import static org.junit.Assert.*;
 
 public class UserManagerTest {
     
-    private static DataSource dataSource;
-    
-    @BeforeClass
-    public static void setUpJNDI() {	
-	System.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.naming.java.javaURLContextFactory");
-	System.setProperty(Context.URL_PKG_PREFIXES, "org.apache.naming");
-
-	dataSource = new OpenChurchUtilityDatasourceForTesting();
-	try {
-	    InitialContext initCtx = new InitialContext();
-	    initCtx.createSubcontext("java:");
-	    initCtx.createSubcontext("java:comp");
-	    initCtx.createSubcontext("java:comp/env");
-	    initCtx.createSubcontext("java:comp/env/jdbc");
-	    initCtx.bind("java:comp/env/jdbc/openchurch", dataSource);
-	} catch(NamingException e){
-	    //do nothing...
-	}
-    }
-    
     
     @Test
-    public void createUser() {
-	EntityManager entityManager = Persistence.createEntityManagerFactory("openchurch_users_test").createEntityManager();
+    public void setUser() {
 	UserManagerBean manager = new UserManagerBean();
-	manager.setEntityManager(entityManager);
 	assertNotNull(manager);
-	User user = null;
-	try {
-	    user = manager.createUser("test-user", "your mother", "rocks", "a@b.com", null, null, null, null, null);
-	} catch (UserManagementException e) {
-	    System.out.println("Dodgy email crept in somehow...");
-	}
+	User user = new User();
+	user.setUsername("test-user");
+	manager.setUser(user);
 	assertNotNull(user);
-	assertTrue(user.getUsername().equals("test-user"));
+	assertTrue(manager.getUser().getUsername().equals("test-user"));
     }
     
     @Test
-    public void findUsers() {
-	//Not sure there's much point continuing with this unit test class - we won't be able to do proper 
-	//persistence outside of the container...
-    }
-    
-    @Test
-    public void editUser(){
-	//see above... 
-    }
-    
-    @Test
-    public void removeUser(){
-	//see above...
+    public void findOutIfAdmin(){
+	UserManagerBean manager = new UserManagerBean();
+	assertNotNull(manager);
+	User user = new User();
+	user.setUsername("test-user");
+	List<Role> roles = new ArrayList<Role>();
+	user.setRoles(roles);
+	manager.setUser(user);
+	assertFalse(manager.isAdmin());
+	Role role = new Role();
+	role.setTitle("admin");
+	roles = new ArrayList<Role>();
+	roles.add(role);
+	user.setRoles(roles);
+	assertTrue(manager.isAdmin());
     }
 
 }
