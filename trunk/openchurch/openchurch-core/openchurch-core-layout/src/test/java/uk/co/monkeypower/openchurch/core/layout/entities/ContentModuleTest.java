abe86ee2bcab2db5c6b9787e1598a613b0f61364
phunnimonkey <phunnimonkey@5ede4246-c789-11dd-a2ab-b939405fd53f>
2009-04-08 21:00:35 +0000

    Added a test which passes for the ContentModule entity.  Also reproduced the testing datasource implementation.  When I get the chance I want to remove this duplication - someone really needs to sponsor me to do this full time! :-)

---
+++ trunk/openchurch/openchurch-core/openchurch-core-layout/src/test/java/uk/co/monkeypower/openchurch/core/layout/entities/ContentModuleTest.java
@@ -0,0 +1,57 @@
+package uk.co.monkeypower.openchurch.core.layout.entities;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.sql.DataSource;
+
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class ContentModuleTest {
+	
+	private static DataSource dataSource;
+	
+	@BeforeClass
+	public static void setUpJNDI() {	
+		System.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.naming.java.javaURLContextFactory");
+		System.setProperty(Context.URL_PKG_PREFIXES, "org.apache.naming");
+
+		dataSource = new OpenChurchUtilityDatasourceForTesting();
+		try {
+			InitialContext initCtx = new InitialContext();
+			initCtx.createSubcontext("java:");
+			initCtx.createSubcontext("java:comp");
+			initCtx.createSubcontext("java:comp/env");
+			initCtx.createSubcontext("java:comp/env/jdbc");
+			initCtx.bind("java:comp/env/jdbc/openchurch", dataSource);
+		} catch(NamingException e){
+			//do nothing...
+		}
+	}
+	
+	@Test
+	public void createContentModule() {
+		EntityManager manager = Persistence.createEntityManagerFactory("openchurch_layout_test").createEntityManager();
+		ContentModule module = new ContentModule();
+		module.setId(1);
+		module.setDeleteable(false);
+		module.setModuleClassName("test");
+		manager.getTransaction().begin();
+		manager.persist(module);
+		manager.getTransaction().commit();
+	}
+	
+	@After
+	public void cleanUpTestData() {
+		EntityManager manager = Persistence.createEntityManagerFactory("openchurch_layout_test").createEntityManager();
+		Query cleanUpQuery = manager.createQuery("delete from ContentModule c where c.moduleClassName = 'test'");
+		cleanUpQuery.executeUpdate();
+	}
+
+}
