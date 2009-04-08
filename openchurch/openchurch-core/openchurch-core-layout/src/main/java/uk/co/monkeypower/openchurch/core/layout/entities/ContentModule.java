662de9b144691cf84a66eb4628a6b480f54376f9
phunnimonkey <phunnimonkey@5ede4246-c789-11dd-a2ab-b939405fd53f>
2009-04-08 21:00:35 +0000

    Added a test which passes for the ContentModule entity.  Also reproduced the testing datasource implementation.  When I get the chance I want to remove this duplication - someone really needs to sponsor me to do this full time! :-)

---
+++ openchurch/openchurch-core/openchurch-core-layout/src/main/java/uk/co/monkeypower/openchurch/core/layout/entities/ContentModule.java
@@ -0,0 +1,40 @@
+package uk.co.monkeypower.openchurch.core.layout.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.TableGenerator;
+
+@Entity
+@Table(name="openchurch_content_modules")
+public class ContentModule {
+	
+	@Id
+    @GeneratedValue(generator="IdTable")
+    @TableGenerator(name="IdTable", allocationSize=2, table = "id_generator", pkColumnName="id_name", valueColumnName="id_value", pkColumnValue="INV_GEN")
+    private long id;
+	private String moduleClassName;
+	private boolean deleteable;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getModuleClassName() {
+		return moduleClassName;
+	}
+	public void setModuleClassName(String moduleClassName) {
+		this.moduleClassName = moduleClassName;
+	}
+	public boolean isDeleteable() {
+		return deleteable;
+	}
+	public void setDeleteable(boolean deleteable) {
+		this.deleteable = deleteable;
+	}
+	
+	
+
+}
