8c9fbd803ffe5cb1c039c7a14372fbf56ec692e7
phunnimonkey <phunnimonkey@5ede4246-c789-11dd-a2ab-b939405fd53f>
2009-04-16 10:43:38 +0000

    Fixed some dodgy indentation - not sure where it came from though...?

--- openchurch/openchurch-core/openchurch-core-layout/src/main/java/uk/co/monkeypower/openchurch/core/layout/entities/ContentModule.java
+++ openchurch/openchurch-core/openchurch-core-layout/src/main/java/uk/co/monkeypower/openchurch/core/layout/entities/ContentModule.java
@@ -10,30 +10,36 @@ import javax.persistence.TableGenerator;
 @Table(name="openchurch_content_modules")
 public class ContentModule {
 	
-	@Id
-    @GeneratedValue(generator="IdTable")
-    @TableGenerator(name="IdTable", allocationSize=2, table = "id_generator", pkColumnName="id_name", valueColumnName="id_value", pkColumnValue="INV_GEN")
+    @Id
+    @GeneratedValue(generator = "IdTable")
+    @TableGenerator(name = "IdTable", allocationSize = 2, table = "id_generator", pkColumnName = "id_name", valueColumnName = "id_value", pkColumnValue = "INV_GEN")
     private long id;
-	private String moduleClassName;
-	private boolean deleteable;
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public String getModuleClassName() {
-		return moduleClassName;
-	}
-	public void setModuleClassName(String moduleClassName) {
-		this.moduleClassName = moduleClassName;
-	}
-	public boolean isDeleteable() {
-		return deleteable;
-	}
-	public void setDeleteable(boolean deleteable) {
-		this.deleteable = deleteable;
-	}
+    private String moduleClassName;
+    private boolean deleteable;
+
+    public long getId() {
+	return id;
+    }
+
+    public void setId(long id) {
+	this.id = id;
+    }
+
+    public String getModuleClassName() {
+	return moduleClassName;
+    }
+
+    public void setModuleClassName(String moduleClassName) {
+	this.moduleClassName = moduleClassName;
+    }
+
+    public boolean isDeleteable() {
+	return deleteable;
+    }
+
+    public void setDeleteable(boolean deleteable) {
+	this.deleteable = deleteable;
+    }
 	
 	
 
