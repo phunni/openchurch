7f2c5861fa9b5d0993034cdb66eb0bbbb7e76390
phunnimonkey <phunnimonkey@5ede4246-c789-11dd-a2ab-b939405fd53f>
2009-05-22 15:04:18 +0000

    Moved the new approach into the other unit tests in the layout module.  I'll get this properly working and tidied up and then move it into the users module.

--- trunk/openchurch/openchurch-core/openchurch-core-layout/src/test/java/uk/co/monkeypower/openchurch/core/layout/entities/PageTest.java
+++ trunk/openchurch/openchurch-core/openchurch-core-layout/src/test/java/uk/co/monkeypower/openchurch/core/layout/entities/PageTest.java
@@ -1,48 +1,41 @@
 package uk.co.monkeypower.openchurch.core.layout.entities;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.sql.DataSource;
 
+
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-import uk.co.monkeypower.openchurch.core.db.OpenChurchUtilityDatasourceForTesting;
 
 
 public class PageTest {
     
-    private static DataSource dataSource;
-    
+    private static EntityManager manager;
+
     @BeforeClass
     public static void setUpJNDI() {
-            System.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.naming.java.javaURLContextFactory");
-            System.setProperty(Context.URL_PKG_PREFIXES, "org.apache.naming");
-            dataSource = new OpenChurchUtilityDatasourceForTesting();
-            try {
-                    InitialContext initCtx = new InitialContext();
-                    initCtx.createSubcontext("java:");
-                    initCtx.createSubcontext("java:comp");
-                    initCtx.createSubcontext("java:comp/env");
-                    initCtx.createSubcontext("java:comp/env/jdbc");
-                    initCtx.bind("java:comp/env/jdbc/openchurch", dataSource);
-            } catch(NamingException e){
-                    //do nothing...
-            } 
+        Properties jdbcProperties = new Properties();
+        try {
+            jdbcProperties.load(MenuItem.class.getClassLoader().getResourceAsStream("db.properties"));
+        } catch (IOException e) {
+            System.out.println("Failed to locate properties file for datasource: " + e);
+        }
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("openchurch_layout_test", jdbcProperties);
+        manager = factory.createEntityManager();
+
     }
     
     @Test
     public void createPage() {
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("openchurch_layout_test");
-        EntityManager manager = factory.createEntityManager();
         Page page = new Page();
         page.setName("test");
         page.setTitle("test");
@@ -59,6 +52,11 @@ public class PageTest {
         manager.merge(page);
         manager.getTransaction().commit();  
         manager.close();
+    }
+    
+    @AfterClass 
+    public static void closeManager(){ 
+        manager.close(); 
     }
 
 }
