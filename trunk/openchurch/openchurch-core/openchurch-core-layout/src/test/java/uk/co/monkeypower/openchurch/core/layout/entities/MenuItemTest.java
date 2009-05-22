8fa7fb96792b8a5367c7e9910764f6e4c9f38e47
phunnimonkey <phunnimonkey@5ede4246-c789-11dd-a2ab-b939405fd53f>
2009-05-22 14:53:10 +0000

    Have taken a completely different approach to db connection in the unit tests.  This will hopefully solve the deadlock problem...

--- trunk/openchurch/openchurch-core/openchurch-core-layout/src/test/java/uk/co/monkeypower/openchurch/core/layout/entities/MenuItemTest.java
+++ trunk/openchurch/openchurch-core/openchurch-core-layout/src/test/java/uk/co/monkeypower/openchurch/core/layout/entities/MenuItemTest.java
@@ -1,69 +1,66 @@
 package uk.co.monkeypower.openchurch.core.layout.entities;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
+import java.io.IOException;
+import java.util.Properties;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
-import javax.sql.DataSource;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-import uk.co.monkeypower.openchurch.core.db.OpenChurchUtilityDatasourceForTesting;
-
 public class MenuItemTest {
+
+    private static EntityManager manager;
+
+    @BeforeClass
+    public static void setUpJNDI() {
+        Properties jdbcProperties = new Properties();
+        try {
+            jdbcProperties.load(MenuItem.class.getClassLoader().getResourceAsStream("db.properties"));
+        } catch (IOException e) {
+            System.out.println("Failed to locate properties file for datasource: " + e);
+        }
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("openchurch_layout_test", jdbcProperties);
+        manager = factory.createEntityManager();
+
+    }
+
+    @Test
+    public void createMenuItem() {
+        MenuItem item = new MenuItem();
+        item.setTitle("test menu item");
+        manager.getTransaction().begin();
+        manager.persist(item);
+        assertTrue(item.getId() != 0);
+        Page page = new Page();
+        page.setTitle("test");
+        page.setName("test");
+        item.setPage(page);
+        manager.persist(page);
+        manager.merge(item);
+        manager.getTransaction().commit();
+    }
+
+    @After
+    public void cleanUpTestData() {
+        EntityManager manager = Persistence.createEntityManagerFactory("openchurch_layout_test").createEntityManager();
+        Query cleanUpQuery = manager.createQuery("delete from MenuItem m where m.title = 'test menu item'");
+        cleanUpQuery.executeUpdate();
+        cleanUpQuery = manager.createQuery("delete from Page p where p.name = 'test'");
+        cleanUpQuery.executeUpdate();
+    }
+
     
-    private static DataSource dataSource;
-	
-	@BeforeClass
-	public static void setUpJNDI() {
-		System.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.naming.java.javaURLContextFactory");
-		System.setProperty(Context.URL_PKG_PREFIXES, "org.apache.naming");
-		dataSource = new OpenChurchUtilityDatasourceForTesting();
-		try {
-			InitialContext initCtx = new InitialContext();
-			initCtx.createSubcontext("java:");
-			initCtx.createSubcontext("java:comp");
-			initCtx.createSubcontext("java:comp/env");
-			initCtx.createSubcontext("java:comp/env/jdbc");
-			initCtx.bind("java:comp/env/jdbc/openchurch", dataSource);
-		} catch(NamingException e){
-			//do nothing...
-		} 
-	}
-	
-	@Test
-	public void createMenuItem() {
-	    EntityManagerFactory factory = Persistence.createEntityManagerFactory("openchurch_layout_test");
-	    EntityManager manager = factory.createEntityManager();
-	    MenuItem item = new MenuItem();
-	    item.setTitle("test menu item");
-	    manager.getTransaction().begin();
-	    manager.persist(item);
-	    assertTrue(item.getId() != 0);
-	    Page page = new Page();
-	    page.setTitle("test");
-	    page.setName("test");
-	    item.setPage(page);
-	    manager.persist(page);
-	    manager.merge(item);	   
-	    manager.getTransaction().commit();
-	    manager.close();
-	}
-	
-	@After
-	public void cleanUpTestData() {
-		EntityManager manager = Persistence.createEntityManagerFactory("openchurch_layout_test").createEntityManager();
-		Query cleanUpQuery = manager.createQuery("delete from MenuItem m where m.title = 'test menu item'");
-		cleanUpQuery.executeUpdate();
-		cleanUpQuery = manager.createQuery("delete from Page p where p.name = 'test'");
-		cleanUpQuery.executeUpdate();
-		manager.close();
-	}
+     @AfterClass 
+     public static void closeManager(){ 
+         manager.close(); 
+     }
+    
 
 }
