7740c3e5a38f953b5eece41e6f52a79d5218a957
phunnimonkey <phunnimonkey@5ede4246-c789-11dd-a2ab-b939405fd53f>
2009-06-26 10:37:05 +0000

    It looks like I might have actually finally fixed this stupid db locks problem...

--- openchurch/openchurch-core/openchurch-core-layout/src/test/java/uk/co/monkeypower/openchurch/core/layout/entities/MenuTest.java
+++ openchurch/openchurch-core/openchurch-core-layout/src/test/java/uk/co/monkeypower/openchurch/core/layout/entities/MenuTest.java
@@ -8,9 +8,7 @@ import java.util.Properties;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
 
-import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -36,43 +34,35 @@ public class MenuTest {
     
     @Test
     public void createMenu() {
-        Menu menu = new Menu();
-        menu.setName("test");
-        manager.getTransaction().begin();
-        manager.persist(menu);
-        manager.getTransaction().commit();
-        assertTrue(menu.getId() >= 1);
-        MenuItem firstItem = new MenuItem();
-        firstItem.setTitle("test");
-        MenuItem secondItem = new MenuItem();
-        secondItem.setTitle("test");
-        MenuItem thirdItem = new MenuItem();
-        thirdItem.setTitle("test");
-        manager.getTransaction().begin();
-        manager.persist(firstItem);
-        manager.persist(secondItem);
-        manager.persist(thirdItem);
-        manager.getTransaction().commit();
-        List<MenuItem> items = new ArrayList<MenuItem>();
-        items.add(firstItem);
-        items.add(secondItem);
-        items.add(thirdItem);
-        menu.setItems(items);
-        manager.getTransaction().begin();
-        manager.merge(menu);
-        manager.getTransaction().commit();
-    }
-    
-    @After
-    public void cleanUp(){
-        manager.getTransaction().begin();
-        Query cleanUpMenuItemsQuery = manager.createQuery("delete from MenuItem m where m.title = 'test'");
-        cleanUpMenuItemsQuery.executeUpdate();
-        manager.getTransaction().commit();
-        manager.getTransaction().begin();
-        Query cleanUpMenusQuery = manager.createQuery("delete from Menu m where m.name = 'test'");
-        cleanUpMenusQuery.executeUpdate();
-        manager.getTransaction().commit();
+        try {
+            Menu menu = new Menu();
+            menu.setName("test");
+            manager.getTransaction().begin();
+            manager.persist(menu);
+            manager.flush();
+            assertTrue(menu.getId() >= 1);
+            MenuItem firstItem = new MenuItem();
+            firstItem.setTitle("test");
+            MenuItem secondItem = new MenuItem();
+            secondItem.setTitle("test");
+            MenuItem thirdItem = new MenuItem();
+            thirdItem.setTitle("test");
+            manager.persist(firstItem);
+            manager.persist(secondItem);
+            manager.persist(thirdItem);
+            manager.flush();
+            List<MenuItem> items = new ArrayList<MenuItem>();
+            items.add(firstItem);
+            items.add(secondItem);
+            items.add(thirdItem);
+            menu.setItems(items);
+            manager.merge(menu);
+            manager.flush();
+        } finally {
+            if(manager.getTransaction().isActive()){
+                manager.getTransaction().rollback();
+            } 
+        }
     }
     
     @AfterClass 
