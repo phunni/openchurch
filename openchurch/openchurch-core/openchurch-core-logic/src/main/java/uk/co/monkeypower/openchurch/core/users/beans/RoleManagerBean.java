ab0a61d0d4dbba928b78fe321ef4c5846f1f8fa5
phunnimonkey <phunnimonkey@5ede4246-c789-11dd-a2ab-b939405fd53f>
2009-07-14 10:21:40 +0000

    Have now added Javadocs to the user management classes

--- openchurch/openchurch-core/openchurch-core-logic/src/main/java/uk/co/monkeypower/openchurch/core/users/beans/RoleManagerBean.java
+++ openchurch/openchurch-core/openchurch-core-logic/src/main/java/uk/co/monkeypower/openchurch/core/users/beans/RoleManagerBean.java
@@ -10,27 +10,46 @@ import javax.persistence.Query;
 
 import uk.co.monkeypower.openchurch.core.users.entities.Role;
 
+/**
+ * The Class RoleManagerBean. This class acts as an entity manager for the Role
+ * Entity
+ */
 @Stateless
 public class RoleManagerBean implements RoleManager {
     
     @PersistenceContext(unitName="openchurch_users")
     private EntityManager entityManager;
-    
+
+    /* (non-Javadoc)
+     * @see uk.co.monkeypower.openchurch.core.users.beans.RoleManager#createRole(uk.co.monkeypower.openchurch.core.users.entities.Role)
+     */
     public void createRole(Role roleToBeCreated) {
 	entityManager.persist(roleToBeCreated);
     }
 
+
+    /* (non-Javadoc)
+     * @see uk.co.monkeypower.openchurch.core.users.beans.RoleManager#getAllRoles()
+     */
     public List<Role> getAllRoles() {
 	Query getAllRolesQuery = entityManager.createQuery("select r from Role r");
 	return getAllRolesQuery.getResultList();
     }
 
+
+    /* (non-Javadoc)
+     * @see uk.co.monkeypower.openchurch.core.users.beans.RoleManager#getRole(java.lang.String)
+     */
     public Role getRole(String roleTitle) {
 	Query getRoleByTitleQuery = entityManager.createQuery("select r from Role r where r.title = ?1");
 	getRoleByTitleQuery.setParameter(1, roleTitle);
 	return (Role) getRoleByTitleQuery.getSingleResult();
     }
 
+
+    /* (non-Javadoc)
+     * @see uk.co.monkeypower.openchurch.core.users.beans.RoleManager#removeRole(uk.co.monkeypower.openchurch.core.users.entities.Role)
+     */
     public void removeRole(Role role) {
 	entityManager.remove(role);
     }
--- openchurch/openchurch-core/openchurch-core-logic/src/main/java/uk/co/monkeypower/openchurch/core/users/beans/exceptions/UserManagementException.java
+++ openchurch/openchurch-core/openchurch-core-logic/src/main/java/uk/co/monkeypower/openchurch/core/users/beans/exceptions/UserManagementException.java
@@ -1,21 +1,43 @@
 package uk.co.monkeypower.openchurch.core.users.beans.exceptions;
 
+/**
+ * The Class UserManagementException.
+ */
 public class UserManagementException extends Exception {
 
     private static final long serialVersionUID = 1L;
 
+    /**
+     * Instantiates a new user management exception.
+     */
     public UserManagementException() {
 	super();
     }
 
+    /**
+     * Instantiates a new user management exception.
+     * 
+     * @param message the message
+     */
     public UserManagementException(String message) {
 	super(message);
     }
 
+    /**
+     * Instantiates a new user management exception.
+     * 
+     * @param cause the cause
+     */
     public UserManagementException(Throwable cause) {
 	super(cause);
     }
 
+    /**
+     * Instantiates a new user management exception.
+     * 
+     * @param message the message
+     * @param cause the cause
+     */
     public UserManagementException(String message, Throwable cause) {
 	super(message, cause);
     }
