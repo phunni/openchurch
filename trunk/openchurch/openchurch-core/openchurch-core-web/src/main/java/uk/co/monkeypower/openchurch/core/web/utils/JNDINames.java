5022754584ab699a2f8ea77f456b8758b463b4d3
phunnimonkey@gmail.com <phunnimonkey@gmail.com@5ede4246-c789-11dd-a2ab-b939405fd53f>
2010-02-15 18:21:07 +0000

    Added a class to contain all the JNDI names

---
+++ trunk/openchurch/openchurch-core/openchurch-core-web/src/main/java/uk/co/monkeypower/openchurch/core/web/utils/JNDINames.java
@@ -0,0 +1,7 @@
+package uk.co.monkeypower.openchurch.core.web.utils;
+
+public class JNDINames {
+	
+	public static String USER_MANAGER = "java:global/openchurch-core-ear/openchurch-core-logic-0.1/UserManagerBean";
+
+}
