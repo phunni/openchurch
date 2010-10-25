6822ece12e3d931af3bd79a612176f13f71ea291
phunnimonkey@gmail.com <phunnimonkey@gmail.com@5ede4246-c789-11dd-a2ab-b939405fd53f>
2010-10-25 11:24:52 +0000

    trying to fix EJB settings in maven.

--- openchurch/openchurch-core/openchurch-core-logic/src/test/java/uk/co/monkeypower/openchurch/core/users/beans/UserManagerTest.java
+++ openchurch/openchurch-core/openchurch-core-logic/src/test/java/uk/co/monkeypower/openchurch/core/users/beans/UserManagerTest.java
@@ -12,37 +12,35 @@ import org.junit.Test;
 import uk.co.monkeypower.openchurch.core.users.entities.Role;
 import uk.co.monkeypower.openchurch.core.users.entities.User;
 
-
-
 public class UserManagerTest {
-	
+
 	@Test
-    public void setUser() {
-	UserManagerBean manager = new UserManagerBean();
-	assertNotNull(manager);
-	User user = new User();
-	user.setUsername("test-user");
-	manager.setUser(user);
-	assertNotNull(user);
-	assertTrue(manager.getUser().getUsername().equals("test-user"));
-    }
-    
-    @Test
-    public void findOutIfAdmin(){
-	UserManagerBean manager = new UserManagerBean();
-	assertNotNull(manager);
-	User user = new User();
-	user.setUsername("test-user");
-	List<Role> roles = new ArrayList<Role>();
-	user.setRoles(roles);
-	manager.setUser(user);
-	assertFalse(manager.isAdmin());
-	Role role = new Role();
-	role.setTitle("admin");
-	roles = new ArrayList<Role>();
-	roles.add(role);
-	user.setRoles(roles);
-	assertTrue(manager.isAdmin());
-    }
+	public void setUser() {
+		UserManagerBean manager = new UserManagerBean();
+		assertNotNull(manager);
+		User user = new User();
+		user.setUsername("test-user");
+		manager.setUser(user);
+		assertNotNull(user);
+		assertTrue(manager.getUser().getUsername().equals("test-user"));
+	}
+
+	@Test
+	public void findOutIfAdmin() {
+		UserManagerBean manager = new UserManagerBean();
+		assertNotNull(manager);
+		User user = new User();
+		user.setUsername("test-user");
+		List<Role> roles = new ArrayList<Role>();
+		user.setRoles(roles);
+		manager.setUser(user);
+		assertFalse(manager.isAdmin());
+		Role role = new Role();
+		role.setTitle("admin");
+		roles = new ArrayList<Role>();
+		roles.add(role);
+		user.setRoles(roles);
+		assertTrue(manager.isAdmin());
+	}
 
 }
