50f86c43f0f85e7f40286f4f3e61911b48d1cfc6
phunnimonkey <phunnimonkey@5ede4246-c789-11dd-a2ab-b939405fd53f>
2011-11-17 16:02:39 +0000

    Created implementation of the PortletDriverServices

---
+++ openchurch/openchurch-core/openchurch-portlet-container/src/main/java/uk/co/monkeypower/openchurch/portlet/OpenChurchPortletDriverServices.java
@@ -0,0 +1,98 @@
+package uk.co.monkeypower.openchurch.portlet;
+
+import javax.portlet.PortalContext;
+
+import org.apache.pluto.container.CCPPProfileService;
+import org.apache.pluto.container.EventCoordinationService;
+import org.apache.pluto.container.FilterManagerService;
+import org.apache.pluto.container.NamespaceMapper;
+import org.apache.pluto.container.PortletEnvironmentService;
+import org.apache.pluto.container.PortletInvokerService;
+import org.apache.pluto.container.PortletPreferencesService;
+import org.apache.pluto.container.PortletRequestContextService;
+import org.apache.pluto.container.PortletURLListenerService;
+import org.apache.pluto.container.RequestDispatcherService;
+import org.apache.pluto.container.UserInfoService;
+import org.apache.pluto.container.driver.PortalAdministrationService;
+import org.apache.pluto.container.driver.PortalDriverServices;
+import org.apache.pluto.container.driver.PortletContextService;
+import org.apache.pluto.container.driver.PortletRegistryService;
+
+public class OpenChurchPortletDriverServices implements PortalDriverServices {
+
+	public PortalContext getPortalContext() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public EventCoordinationService getEventCoordinationService() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PortletRequestContextService getPortletRequestContextService() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public FilterManagerService getFilterManagerService() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PortletURLListenerService getPortletURLListenerService() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PortletPreferencesService getPortletPreferencesService() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PortletEnvironmentService getPortletEnvironmentService() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PortletInvokerService getPortletInvokerService() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public UserInfoService getUserInfoService() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public NamespaceMapper getNamespaceMapper() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public CCPPProfileService getCCPPProfileService() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public RequestDispatcherService getRequestDispatcherService() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PortletContextService getPortletContextService() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PortletRegistryService getPortletRegistryService() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PortalAdministrationService getPortalAdministrationService() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
